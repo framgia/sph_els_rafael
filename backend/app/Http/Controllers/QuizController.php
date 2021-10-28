77b8b6e3234dff3f0dbfbb09423f40e1498b42ae
rafael <rafaelllparaynooo23@gmail.com>
2021-10-28 17:09:28 +0800

    add validate

--- backend/app/Http/Controllers/QuizController.php
+++ backend/app/Http/Controllers/QuizController.php
@@ -16,6 +16,11 @@ class QuizController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
         return Quiz::create($request->all());
     }
     // public function store
